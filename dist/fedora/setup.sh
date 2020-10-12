7546661b523428840bf1334639d33861190ac539
Zhibin Li <haoyouab@gmail.com>
2020-10-13 01:53:32 +0800

    add setup for powerline git status
    Signed-off-by: Zhibin Li <haoyouab@gmail.com>

--- dist/fedora/setup.sh
+++ dist/fedora/setup.sh
@@ -23,6 +23,15 @@ echo "Setuping Powerline ..."
 sudo dnf install powerline powerline-fonts -y
 grep -q "powerline-daemon" ~/.bashrc
 [ $? -ne 0 ] && cat powerline/bashrc >> ~/.bashrc
-source ~/.bashrc
+sudo dnf install python-pip
+pip install powerline-gitstatus --user
+POWERLINE_LOCAL_CONFIG=~/.config/powerline/
+POWERLINE_GLOBAL_CONFIG=/etc/xdg/powerline/config_files/
+mkdir -p $POWERLINE_LOCAL_CONFIG
+sudo cp -r $POWERLINE_GLOBAL_CONFIG/* $POWERLINE_LOCAL_CONFIG
+sudo chown $USER:$USER $POWERLINE_LOCAL_CONFIG -R
+cp -r $DIST_ROOT/powerline/{colorschemes,themes} $POWERLINE_LOCAL_CONFIG
+powerline-daemon --replace
+exec bash
 echo "Powerline Setup Done"
 echo "=================================================================================="
--- dist/ubuntu/setup.sh
+++ dist/ubuntu/setup.sh
@@ -2,6 +2,8 @@
 
 echo -e "\nEntering Ubuntu configuration directory ...\n"
 
+sudo apt update
+
 echo "=================================================================================="
 echo "Setuping Vim ..."
 sudo apt install vim -y
@@ -23,6 +25,15 @@ echo "Setuping Powerline ..."
 sudo apt install powerline fonts-powerline -y
 grep -q "powerline-daemon" ~/.bashrc
 [ $? -ne 0 ] && cat $DIST_ROOT/powerline/bashrc >> ~/.bashrc
-source ~/.bashrc
+sudo apt install python-pip
+pip install powerline-gitstatus --user
+POWERLINE_LOCAL_CONFIG=~/.config/powerline/
+POWERLINE_GLOBAL_CONFIG=/usr/share/powerline/config_files/
+mkdir -p $POWERLINE_LOCAL_CONFIG
+sudo cp -r $POWERLINE_GLOBAL_CONFIG/* $POWERLINE_LOCAL_CONFIG
+sudo chown $USER:$USER $POWERLINE_LOCAL_CONFIG -R
+cp -r $DIST_ROOT/powerline/{colorschemes,themes} $POWERLINE_LOCAL_CONFIG
+powerline-daemon --replace
+exec bash
 echo "Powerline Setup Done"
 echo "=================================================================================="
