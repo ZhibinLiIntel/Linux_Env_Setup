d36705ff1fc7b748463ed48512de7ea182640483
Zhibin Li <zhibin.li@intel.com>
2021-12-05 16:22:31 +0800

    support GDB dashboard and upgrade

---
+++ dist/Fedora/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo -e "${GREEN}\nSetting up Fedora...\n${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setting up Vim ..."
+sudo dnf install vim -y
+cp -r $DIST_ROOT/vim/vim ~/.vim
+cp $DIST_ROOT/vim/vimrc ~/.vimrc
+echo "Vim Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setting up Tmux ..."
+sudo dnf install tmux -y
+cp $DIST_ROOT/tmux/tmux.conf ~/.tmux.conf
+cp $DIST_ROOT/tmux/tmux.conf.local ~/.tmux.conf.local
+cp $DIST_ROOT/tmux/tmux.conf.debug ~/.tmux.conf.debug
+echo "Tmux Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setting up Powerline ..."
+sudo dnf install powerline powerline-fonts -y
+grep -q "powerline-daemon" ~/.bashrc
+[ $? -ne 0 ] && cat powerline/bashrc >> ~/.bashrc
+sudo dnf install python-pip
+pip install powerline-gitstatus --user
+POWERLINE_LOCAL_CONFIG=~/.config/powerline/
+POWERLINE_GLOBAL_CONFIG=/etc/xdg/powerline/config_files/
+mkdir -p $POWERLINE_LOCAL_CONFIG
+sudo cp -r $POWERLINE_GLOBAL_CONFIG/* $POWERLINE_LOCAL_CONFIG
+sudo chown $USER:$USER $POWERLINE_LOCAL_CONFIG -R
+cp -r $DIST_ROOT/powerline/{colorschemes,themes} $POWERLINE_LOCAL_CONFIG
+powerline-daemon --replace
+exec bash
+echo "Powerline Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setting up GDB"
+sudo dnf install gdb -y
+pip install pygments
+cp $DIST_ROOT/gdb/.gdbinit ~
+mkdir -p ~/.gdbinit.d
+echo -e "${GREEN}==================================================================================${RST}"
---
+++ dist/Ubuntu/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo -e "${GREEN}\nSetting up Ubuntu...\n${RST}"
+
+sudo apt update
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setuping Vim ..."
+sudo apt install vim -y
+cp -r $DIST_ROOT/vim/vim ~/.vim
+cp $DIST_ROOT/vim/vimrc ~/.vimrc
+echo "Vim Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setuping Tmux ..."
+sudo apt install tmux -y
+cp $DIST_ROOT/tmux/tmux.conf ~/.tmux.conf
+cp $DIST_ROOT/tmux/tmux.conf.local ~/.tmux.conf.local
+cp $DIST_ROOT/tmux/tmux.conf.debug ~/.tmux.conf.debug
+echo "Tmux Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setuping Powerline ..."
+sudo apt install powerline fonts-powerline -y
+grep -q "powerline-daemon" ~/.bashrc
+[ $? -ne 0 ] && cat $DIST_ROOT/powerline/bashrc >> ~/.bashrc
+sudo apt install python-pip -y
+pip install powerline-gitstatus --user
+POWERLINE_LOCAL_CONFIG=~/.config/powerline/
+POWERLINE_GLOBAL_CONFIG=/usr/share/powerline/config_files/
+mkdir -p $POWERLINE_LOCAL_CONFIG
+sudo cp -r $POWERLINE_GLOBAL_CONFIG/* $POWERLINE_LOCAL_CONFIG
+sudo chown $USER:$USER $POWERLINE_LOCAL_CONFIG -R
+cp -r $DIST_ROOT/powerline/{colorschemes,themes} $POWERLINE_LOCAL_CONFIG
+powerline-daemon --replace
+exec bash
+echo "Powerline Setup Done"
+echo -e "${GREEN}==================================================================================${RST}"
+
+echo -e "${GREEN}==================================================================================${RST}"
+echo "Setting up GDB"
+sudo dnf install gdb -y
+pip install pygments
+cp $DIST_ROOT/gdb/.gdbinit ~
+mkdir -p ~/.gdbinit.d
+echo -e "${GREEN}==================================================================================${RST}"
